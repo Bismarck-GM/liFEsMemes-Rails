071e1e5a43787181e97756f11673d7614e3cf492
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-09-14 13:00:06 -0300

    Add partials views and helper logic for Log In / Log Out links.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,18 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :require_login
+  helper_method :current_user, :require_login, :user_is_logged?
 
   def current_user
     User.find(session[:user_id])
   end
 
+  def user_is_logged?
+    !session[:user_id].nil?
+  end
+
   def require_login
     unless session[:user_id]
-      flash[:notice] = ["You must be loged in."]
-      redirect_to root_path
+      flash[:notice] = ["Error. You must be logged in before doing that action."]
+      redirect_to new_session_path
     end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def render_navbar_links
+    user_is_logged? ? 'partials/welcome_exit_links' : 'partials/unlogged_links'
+  end
 end
