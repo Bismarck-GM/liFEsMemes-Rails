aebb2205825b683d01c7f5dcb4da8a769cea065a
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-09-17 14:32:16 -0300

    Finish homepage stylings and refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def render_navbar_links
     user_is_logged? ? 'partials/welcome_exit_links' : 'partials/unlogged_links'
   end
+
+  def is_page_active(path)
+    "active" if current_page?(path)
+  end
 end
