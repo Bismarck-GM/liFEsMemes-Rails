efcf4f5da0a9f244b1981675d78b692db8772454
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-09-21 21:01:34 -0300

    Add seed for heroku deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,5 @@ Category.create name: 'Gaming', priority: 1, image: 'https://www.fosi.org/media/
 Category.create name: 'Classic', priority: 2, image: 'https://ligalive.net/wp-content/uploads/2020/03/no-football-on-tv-corona-meme-1200x1200.png'
 Category.create name: 'New', priority: 3, image: 'https://duffy.agency/wp-content/uploads/2019/05/example-of-Dos-Equis-meme.png'
 Category.create name: 'Programming', priority: 4, image: 'https://digitalsynopsis.com/wp-content/uploads/2018/10/funny-memes-for-designers-2.jpg'
+bender = User.create name: 'Bender', email: 'geromorla@yahoo.com.ar', password: '123456', password_confirmation: '123456'
+bender.articles.create title: 'Bender', text: 'I\'m the greatest character ever created. Bite my shiny metal ass', image: File.new(Rails.root.join('app', 'assets', 'images', 'demo', 'bender-drink.jpg')), category_ids: [1, 2, 3, 4]
